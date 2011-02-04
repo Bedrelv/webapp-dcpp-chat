c6eaf34355d981dafefb3221c50fbd7677967e97
Yurtaev Egor <yurtaev.egor@gmail.com>
2011-02-05 01:12:55 +0600

    added: chat.js (future lib)

---
+++ src/static/js/chat.js
@@ -0,0 +1,28 @@
+/**
+ * Created by .
+ * User: Yurtaev
+ * Date: 05.02.11
+ * Time: 0:58
+ */
+
+var chat = {
+
+    'connect': function() {
+
+    },
+
+    'disconnect': function() {
+
+    }
+};
+
+chat.evets = {
+
+    'on_global_message': function(json) {
+
+    },
+
+    'on_private_message': function(json) {
+
+    }
+}
